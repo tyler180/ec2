12d8e6a9c23c8e65304802f99d5f248259bf2c40
Tyler McLean <tyler_180@hotmail.com>
2024-12-10 13:34:29 -0700

    no hcp

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ terraform {
   }
 }
 
-provider "hcp" {}
+# provider "hcp" {}
 
 provider "aws" {
   region = var.region
@@ -30,24 +30,24 @@ provider "aws" {
 #   channel     = "production"
 # }
 
-data "hcp_packer_version" "testing" {
-  bucket_name   = "learn-packer-ubuntu"
-  channel_name = "latest"
-}
+# data "hcp_packer_version" "testing" {
+#   bucket_name   = "learn-packer-ubuntu"
+#   channel_name = "latest"
+# }
 
-data "hcp_packer_artifact" "learn-packer-ubuntu" {
-  bucket_name   = "learn-packer-ubuntu"
-  channel_name  = "latest"
-  platform      = "aws"
-  region        = "us-east-2"
-}
+# data "hcp_packer_artifact" "learn-packer-ubuntu" {
+#   bucket_name   = "learn-packer-ubuntu"
+#   channel_name  = "latest"
+#   platform      = "aws"
+#   region        = "us-east-2"
+# }
 
-data "hcp_packer_image" "ubuntu_us_east_2" {
-  bucket_name    = "learn-packer-ubuntu"
-  cloud_provider = "aws"
-  iteration_id   = data.hcp_packer_iteration.ubuntu.ulid
-  region         = "us-east-2"
-}
+# data "hcp_packer_image" "ubuntu_us_east_2" {
+#   bucket_name    = "learn-packer-ubuntu"
+#   cloud_provider = "aws"
+#   iteration_id   = data.hcp_packer_iteration.ubuntu.ulid
+#   region         = "us-east-2"
+# }
 
 
 resource "aws_instance" "app_server" {
