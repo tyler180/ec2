98bf4b7d46c1900be5e302b82ee0bf6b183c0a71
Tyler McLean <tyler_180@hotmail.com>
2024-12-10 13:39:48 -0700

    sqs

--- main.tf
+++ main.tf
@@ -50,13 +50,26 @@ provider "aws" {
 # }
 
 
-resource "aws_instance" "app_server" {
-  # for_each = toset(local.ec2_instance_types)
-  ami           = "ami-0033fe31db68da96a"
-  instance_type = "t2.micro"
+# resource "aws_instance" "app_server" {
+#   # for_each = toset(local.ec2_instance_types)
+#   ami           = "ami-0033fe31db68da96a"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "ExampleAppServerInstance"
+#   }
+# }
+
+resource "aws_sqs_queue" "terraform_queue" {
+  name                      = "terraform-example-queue"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Environment = "production"
   }
 }
 
